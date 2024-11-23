9c31cecf90f002c23dbab7f09a08e72f9f9b0644
hk4u-dxv <gxra.dev@gmail.com>
2024-11-22 20:28:30 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,21 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src',
+        '@components': '/src/components',
+        '@componentsUI': '/src/components/ui',
+        '@componentsSections': '/src/components/sections',
+        '@componentsLayout': '/src/components/layout',
+        '@layouts': '/src/layouts',
+        '@pages': '/src/pages',
+        '@scripts': '/src/scripts',
+        '@assets': '/src/assets',
+        '@styles': '/src/styles'
+      }
+    }
+  }
 });
